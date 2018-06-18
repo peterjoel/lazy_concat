4b623e36bffc6ece42e6ea2f38f7a4d0cba32983
Peter <peterjoel@gmail.com>
2018-06-19 00:00:06 +0200

    Move self for mutability, encode fragmentation in types

--- src/lib.rs
+++ src/lib.rs
@@ -1,93 +1,134 @@
 use std::{
     fmt::{self, Debug, Display, Formatter},
-    borrow::Cow,
-    mem
+    borrow::{Cow, Borrow},
+    marker::PhantomData,
 };
+
+pub struct Normalized;
+pub struct Fragmented;
 
 pub trait Concat<T = Self>
 where T: ?Sized,
 {
-    fn concat(self, other: &T) -> Self;
+    type Output;
+    fn concat(self, other: T) -> Self::Output;
 }
 
 impl Concat for String {
-    fn concat(mut self, other: &String) -> String {
+    type Output = String;
+    fn concat(mut self, other: Self) -> String {
         self.push_str(&other);
         self
     }
 }
 
-impl Concat<str> for String {
+impl<'a> Concat<&'a str> for String {
+    type Output = String;
     fn concat(mut self, other: &str) -> String {
         self.push_str(&other);
         self
     }
 }
 
-impl<'a> Concat for Cow<'a, str> {
-    fn concat(self, other: &Cow<'a, str>) -> Cow<'a, str> {
-        let mut owned = self.into_owned();
+impl<'a> Concat<&'a str> for &'a str {
+    type Output = String;
+    fn concat(self, other: &str) -> String {
+        let mut owned = self.to_owned();
         owned.push_str(&other);
-        Cow::Owned(owned)
+        owned
     }
 }
 
-
-pub struct LazyConcat<'a, T> 
-where
-    T: ToOwned + 'a + ?Sized,
-{
-    root: Option<T::Owned>,
-    fragments: Vec<&'a T>,
+impl<'a> Concat<&'a str> for Cow<'a, str> {
+    type Output = Cow<'a, str>;
+    fn concat(self, other: &'a str) -> Cow<'a, str> {
+        let owned = self.into_owned();
+        Cow::Owned(owned.concat(other))
+    }
 }
 
-impl<'a, T> LazyConcat<'a, T> 
-where
-    T: ToOwned + 'a + ?Sized,
-    T::Owned: Concat<T>
+impl<'a> Concat<String> for Cow<'a, str> {
+    type Output = Cow<'a, str>;
+    fn concat(self, other: String) -> Cow<'a, str> {
+        let owned = self.into_owned();
+        Cow::Owned(owned.concat(other.borrow()))
+    }
+}
+
+impl<'a> Concat<Cow<'a, str>> for Cow<'a, str> {
+    type Output = Cow<'a, str>;
+    fn concat(self, other: Cow<'a, str>) -> Cow<'a, str> {
+        let owned = self.into_owned();
+        Cow::Owned(owned.concat(other.borrow()))
+    }
+}
+
+impl<'a, T> Concat<&'a [T]> for Vec<T> 
+    where 
+        T: Clone 
 {
-    pub fn new() -> LazyConcat<'a, T> {
-        LazyConcat { root: None, fragments: Vec::new() }
+    type Output = Vec<T>;
+    fn concat(mut self, other: &'a [T]) -> Vec<T> {
+        self.extend_from_slice(other);
+        self
+    }
+}
+
+pub struct LazyConcat<T, B, F = Fragmented> 
+where
+    T: Concat<B>
+{
+    root: T,
+    fragments: Vec<B>,
+    phantom: PhantomData<F>,
+}
+
+pub type NormalizedConcat<T, B> = LazyConcat<T, B, Normalized>;
+
+impl<T, B> LazyConcat<T, B, Fragmented> 
+where
+    T: Concat<B, Output = T>
+{
+    pub fn new(initial: T) -> LazyConcat<T, B, Fragmented> {
+        LazyConcat { root: initial, fragments: Vec::new(), phantom: PhantomData }
+    }
+    
+    pub fn expecting_num_fragments(initial: T, n: usize) -> LazyConcat<T, B, Fragmented> {
+        LazyConcat { root: initial, fragments: Vec::with_capacity(n), phantom: PhantomData }
+    }
+}
+
+impl<T, B, F> LazyConcat<T, B, F> 
+where
+    T: Concat<B, Output = T>
+{
+    fn change_type<X>(self) -> LazyConcat<T, B, X> {
+        LazyConcat { root: self.root, fragments: self.fragments, phantom: PhantomData }
+    } 
+
+    pub fn normalize(mut self) -> LazyConcat<T, B, Normalized> {
+        {
+            let fragments = self.fragments.drain(..);
+            self.root = fragments.fold(self.root, |agg, frag| agg.concat(frag));
+        }
+        self.change_type()
     }
 
-    pub fn expecting_num_fragments(n: usize) -> LazyConcat<'a, T> {
-        LazyConcat { root: None, fragments: Vec::with_capacity(n) }
-    }
-
-    pub fn normalize(&mut self) {
-        let mut fragments = self.fragments.drain(..);
-        let first = if let Some(normal) = mem::replace(&mut self.root, None) {
-            normal
-        } else {
-            fragments.next().map(|fragment| fragment.to_owned()).unwrap()
-        };
-        let normal = fragments.fold(first, |agg, frag| agg.concat(&frag));
-        mem::replace(&mut self.root, Some(normal));
-    }
-
-    pub fn concat<'b: 'a>(&mut self, fragment: &'b T) {
+    pub fn concat(mut self, fragment: B) -> LazyConcat<T, B, Fragmented> {
         self.fragments.push(fragment);
+        self.change_type()
     }
-
-    pub fn is_normal(&self) -> bool {
-        self.fragments.len() == 0
-    }
-
-    pub fn get_normal(&mut self) -> &Option<T::Owned> {
-        self.normalize();
-        &self.root
-   }
 }
 
-impl<'a, T> Debug for LazyConcat<'a, T> 
+impl<T, B, F> Debug for LazyConcat<T, B, F> 
 where
-    T: ToOwned + ?Sized + Debug,
-    T::Owned: Debug,
+    T: Concat<B, Output = T> + Debug,
+    B: Debug,
 {
     fn fmt(&self, f: &mut Formatter) -> fmt::Result {
         f.write_str("LazyConcat { ")?;
         write!(f, ", {:?}", &self.root)?;
-        for frag in self.fragments.iter() {
+        for frag in &self.fragments {
             write!(f, ", {:?}", &frag)?;
         }
         f.write_str(" }")?;
@@ -95,17 +136,14 @@ where
     }
 }
 
-impl<'a, T> Display for LazyConcat<'a, T>
+impl<T, B, F> Display for LazyConcat<T, B, F>
 where
-    T: ToOwned + ?Sized + Display,
-    T::Owned: Display,
+    T: Concat<B, Output = T> + Display,
+    B: Display,
 {
-    // TODO: Normalize first - will require interior mutability or consume-self
     fn fmt(&self, f: &mut Formatter) -> fmt::Result {
-        if let Some(root) = &self.root {
-            write!(f, "{}", root)?;
-        }
-        for frag in self.fragments.iter() {
+        write!(f, "{}", self.root)?;
+        for frag in &self.fragments {
             write!(f, "{}", &frag)?;
         }
         Ok(())
@@ -122,17 +160,14 @@ mod tests {
         let a = "hel";
         let b = "lo the";
         let c = "re!";
-        let mut lz: LazyConcat<str> = LazyConcat::new();
-        lz.concat(a);
-        lz.concat(b);
-        lz.concat(c);
+        let lz = LazyConcat::new(String::from(""))
+            .concat(a)
+            .concat(b)
+            .concat(c);
 
-        assert_eq!(false, lz.is_normal());
         assert_eq!("hello there!", format!("{}", lz));
-        assert_eq!(false, lz.is_normal());
 
-        lz.normalize();
-        assert_eq!(true, lz.is_normal());
+        let lz = lz.normalize();
         assert_eq!("hello there!", format!("{}", lz));
     }
 }
